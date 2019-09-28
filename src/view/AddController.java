e22d6be14f166e89f8cfe227c63aab1e43da8815
maicodes <49761401+maicodes@users.noreply.github.com>
2019-09-27 20:41:47 -0400

    Add files via upload

---
+++ src/view/AddController.java
@@ -0,0 +1,25 @@
+package view;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import classes.Song;
+
+public class AddController {
+	
+	
+	@FXML
+	void addSong() {}
+	
+}
