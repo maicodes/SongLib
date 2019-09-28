1a40939a20a3f5f631b0b902f766187ccace44d3
maicodes <49761401+maicodes@users.noreply.github.com>
2019-09-28 07:25:59 -0400

    Add files via upload

---
+++ src/classes/SongList.java
@@ -0,0 +1,64 @@
+package classes;
+
+import java.util.TreeMap;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class SongList {
+	
+	static private TreeMap<String, TreeMap<String, Song> > songListTM = new TreeMap<String, TreeMap<String, Song> >();
+	private static ObservableList<Song> obsList = FXCollections.observableArrayList();
+	
+	private String getTitle() {
+		return"";
+	}
+	
+	private String getArtist() {
+		return"";
+	}
+	
+	private String getAlbum() {
+		return "";
+	}
+	
+	private int getYear() {
+		return 0;
+	}
+	
+	public static ObservableList<Song> getSongs() {
+		
+		return obsList;
+	}
+	
+	public static boolean editSong(Song song) {
+		String title = song.title;
+		String artist = song.artist;
+		if ( songListTM.containsKey(title) && songListTM.get(title).containsKey(artist) ) {
+			
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean addNewSong(Song newSong) {
+		String title = newSong.title;
+		String artist = newSong.artist;
+		if ( songListTM.containsKey(title) ) {
+			if (songListTM.get(title).containsKey(artist) ) {
+				return false;
+			}
+			songListTM.get(title).put(artist, newSong);
+		} else {
+			TreeMap<String, Song> tm = new TreeMap<String, Song>();
+			tm.put(artist, newSong);
+			songListTM.put(title, tm );
+		}
+		return true;
+	}
+	
+	private boolean deleteSong() {
+		return true;
+	}
+	
+}
